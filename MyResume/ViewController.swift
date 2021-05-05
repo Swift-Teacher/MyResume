f8682156574b26f1ba82b7e7cfa6c8ccd91c351b
Swift-Teacher <brian@swiftteacher.org>
2021-05-05 13:33:38 -0400

    Completed up to step #35. Added the two secondary screens and connected the Call Me button to VC.swift. CallMe button is coded.

--- MyResume/ViewController.swift
+++ MyResume/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // MARK: - Instance Properties
+    
+    // #24
+    let phoneNumber = "3305555555"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    // MARK: - IB Actions
+    // #23
+    @IBAction func callMe(_ sender: Any) {
+            // #25 - all code and comments in this code block
+            // this line of code enables a physical device to call our phone number so the person can leave us a voicemail
+            if let url = NSURL(string: "tel://\(phoneNumber)") {
+                UIApplication.shared.open(url as URL, options: [:], completionHandler: nil)
+                // this line of code demonstrates that the call button will work and actually call the number. This is inserted to demo functionality
+                // since the Simulator cannot make a phone call. This line will print a string to the console.
+                print("The phone is calling my number. Voicemail will be triggered automatically.")
+            }
+        }
+    
 
 }
 
