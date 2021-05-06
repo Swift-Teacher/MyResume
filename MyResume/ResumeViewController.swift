187e60d1938348e2791f9f2f4622cdcd1b10a6a0
Brian Foutty <8526059+Swift-Teacher@users.noreply.github.com>
2021-05-06 18:54:59 -0400

    Added ResumeViewController and constant to hold resume in memory.

---
+++ MyResume/ResumeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ResumeViewController.swift
+//  MyResume
+//
+//  Created by Brian Foutty on 5/6/21.
+//
+
+import UIKit
+
+class ResumeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
